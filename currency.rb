6166d69dcb415b15dc97c4478142e61256f7bdff
Sherod Taylor <sherodtaylor@gmail.com>
2013-04-06 00:49:50 -0700

    finished calc with tdd

--- currency.rb
+++ currency.rb
@@ -6,6 +6,10 @@ require 'pry'
 require 'test/unit'
 
 class ConvertCurrency
+  def initialize
+    @usd = 1
+  end
+
   def get_exchange_rate(currency_type)
   @file = open('http://openexchangerates.org/api/latest.json?app_id=1c4a7b48db7148ebbfccdf5f072f5ae7')
   @j = JSON.load(@file.read)
@@ -13,12 +17,13 @@ class ConvertCurrency
   end
   
   def convert_to_foreign(currency, amount)
-    usd = 1
     foreign = get_exchange_rate(currency)
-    foreign * amount.to_i * usd 
+   # (@usd / (foreign * amount.to_i))
+    (amount.to_i * (@usd / foreign))
   end
   def convert_to_usd(currency, amount)
-
+    foreign = get_exchange_rate(currency)
+    foreign * amount.to_i * @usd
   end
 end
 
@@ -30,9 +35,18 @@ class ConvertCurrencyTest < Test::Unit::TestCase
     @compare_c = @j['rates']
   end
   def test_gbp_to_foreign
-    assert_equal (@compare_c['GBP'] * 1), @converter.convert_to_foreign("gbp", 1)
+    assert_equal ((1 / @compare_c['GBP']) * 5), @converter.convert_to_foreign("gbp", 5)
   end
   def test_egp_to_foreign
-    assert_equal (@compare_c['EGP'] * 1), @converter.convert_to_foreign("egp", 1)
+    assert_equal ((1 / @compare_c['EGP']) * 5), @converter.convert_to_foreign("egp", 5)
+  end
+  def test_usd_to_gbp
+    assert_equal (@compare_c['GBP'] * 2), @converter.convert_to_usd("gbp", 2)
+  end
+  def test_usd_to_gbp
+    assert_equal (@compare_c['GBP'] * 9), @converter.convert_to_usd("gbp", 9)
+  end
+  def test_usd_to_eur
+    assert_equal (@compare_c['EUR'] * 323), @converter.convert_to_usd("eur", 323)
   end
 end
